ea6d3c606393157a6b46ed466219f2322b6545db
mac <1261914531@qq.com>
2016-09-29 19:50:18 +0800

    CellAnimation
    单元格的一个弹出动画(很喜欢这种效果,所以就记录下来了)

---
+++ CellAnimation/8.单元格动画/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  8.单元格动画
+//
+//  Created by mac on 16/9/17.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    var tableview : UITableView?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        animationTable()
+    }
+    //动画
+    func animationTable() {
+        self.tableview?.reloadData()
+        let cells = tableview?.visibleCells
+        let tableHeight: CGFloat = (tableview?.bounds.size.height)!
+        for i in cells! {
+            let cell: UITableViewCell = i as UITableViewCell
+            cell.transform = CGAffineTransform(translationX:0,y:tableHeight)
+            
+        }
+        
+        var index = 0
+        for a in cells! {
+            let cell: UITableViewCell = a as UITableViewCell
+            UIView.animate(withDuration: 1.5, delay: 0.05*Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: { 
+                cell.transform = CGAffineTransform(translationX:0,y:0)
+                }, completion: nil)
+            index += 1
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    tableview = UITableView.init(frame: UIScreen.main.bounds, style: UITableViewStyle.plain)
+    tableview?.delegate = self
+    tableview?.dataSource = self
+    self.view.addSubview(tableview!)
+    
+    
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellID = "CELLID"
+        var cell = tableview?.dequeueReusableCell(withIdentifier: cellID)
+        if cell == nil {
+            cell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: cellID)
+            cell?.selectionStyle = UITableViewCellSelectionStyle.none
+
+        }
+        cell?.backgroundColor =  colorforIndex((indexPath as NSIndexPath).row)
+        
+        
+        return cell!
+    }
+    
+    func colorforIndex(_ index: Int) -> UIColor {
+        //arc4random()%10
+        let itemCount = 8
+        let color = (CGFloat(index) / CGFloat(itemCount)) * 0.6
+        return UIColor(red: color, green: 0.0, blue: 1.0, alpha: 1.0)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let nextVC = NextViewController()
+        self.present(nextVC, animated: true, completion: nil)
+
+    }
+    
+    
+    
+}
+
